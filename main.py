dfd013875e8b7e7b1f49305665e279ff621575db
Supraja-chava <145627403+Supraja-chava@users.noreply.github.com>
2023-12-03 17:10:18 -0600

    Added Book Page Functionalities
    Added Book Page for displaying all Books, update Book details and delete Book. Also added Front End UI.

--- main.py
+++ main.py
@@ -132,6 +132,110 @@ def delete_publisher(publisher_id):
     return redirect('/publishers')
 
 
+# Display all books
+@app.route('/books')
+def display_books():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Books')
+    books = cursor.fetchall()
+    return render_template('books.html', books=books)
+
+# Add a new book
+@app.route('/books/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        conn = get_db()
+        cursor = conn.cursor()
+        title = request.form['title']
+        author = request.form['author']
+        price = request.form['price']
+        published_year = request.form['published_year']
+        publisher_id = request.form['publisher_id']  # Get the selected publisher_id from the form
+
+        # Generate a 6-digit unique book ID
+        book_id = str(uuid.uuid4().int)[:6]
+
+        cursor.execute('INSERT INTO Books (book_id, book_title, book_author, book_price, published_year, publisher_id) VALUES (?, ?, ?, ?, ?, ?)',
+                       (book_id, title, author, price, published_year, publisher_id))
+        conn.commit()
+        return redirect('/books')
+
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT publisher_id, publisher_name FROM Publisher')
+    publishers = cursor.fetchall()  # Fetch all the publishers to display in the dropdown select field
+
+    return render_template('add_book.html', publishers=publishers)
+
+# Update a book
+@app.route('/books/update/<int:book_id>', methods=['GET', 'POST'])
+def update_book(book_id):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        price = request.form['price']
+        published_year = request.form['published_year']
+        publisher_id = request.form['publisher_id']  # Get the selected publisher_id from the form
+
+        cursor.execute('UPDATE Books SET book_title=?, book_author=?, book_price=?, published_year=?, publisher_id=? WHERE book_id=?',
+                       (title, author, price, published_year, publisher_id, book_id))
+        conn.commit()
+        return redirect('/books')
+
+    cursor.execute('SELECT * FROM Books WHERE book_id=?', (book_id,))
+    book = cursor.fetchone()
+
+    cursor.execute('SELECT publisher_id, publisher_name FROM Publisher')
+    publishers = cursor.fetchall()  # Fetch all the publishers to display in the dropdown select field
+
+    return render_template('update_book.html', book=book, publishers=publishers)
+
+# Delete a book
+@app.route('/books/delete/<int:book_id>', methods=['POST'])
+def delete_book(book_id):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute('DELETE FROM Books WHERE book_id=?', (book_id,))
+    conn.commit()
+    return redirect('/books')
+
+# Search books
+@app.route('/books/search', methods=['GET'])
+def search_books():
+    search_query = request.args.get('query', '').strip()
+
+    if not search_query:
+        # If the search query is empty, display all books
+        return redirect('/books')
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Perform the search query using SQL LIKE clause to filter the results
+    cursor.execute('SELECT * FROM Books WHERE book_id LIKE ? OR book_title LIKE ? OR book_author LIKE ?', ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
+    books = cursor.fetchall()
+
+    return render_template('books.html', books=books)
+
+# Function to open the modal and load publisher details
+@app.route('/publishers/<int:publisher_id>')
+def open_publisher_details(publisher_id):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Fetch publisher details
+    cursor.execute('SELECT * FROM Publisher WHERE publisher_id=?', (publisher_id,))
+    publisher = cursor.fetchone()
+
+    # Render the publisher details template
+    return render_template('publisher_details.html', publisher=publisher)
+
+
 
 
     
